9ec7e1dc31e8b18ed1a4c8a96669a2996acb0235
Yuri Davello <yuridavello@hotmail.com>
2021-12-01 23:51:33 -0300

    Initial commit

---
+++ src/main/java/com/ifsp/pwe_projetofinal/PweProjetoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.ifsp.pwe_projetofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PweProjetoFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PweProjetoFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ifsp/pwe_projetofinal/PweProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsp.pwe_projetofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PweProjetoFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
