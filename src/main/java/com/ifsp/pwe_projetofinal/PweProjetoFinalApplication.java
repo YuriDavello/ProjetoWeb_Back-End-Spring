048cdf8c171a8373680d2b9dacef2439f93a92ba
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-10 18:28:15 -0300

    Refactoring project's structure

--- src/main/java/com/ifsp/pwe_projetofinal/PweProjetoFinalApplication.java
+++ src/main/java/com/ifsp/pwe_projetofinal/PweProjetoFinalApplication.java
@@ -2,20 +2,13 @@ package com.ifsp.pwe_projetofinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PweProjetoFinalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PweProjetoFinalApplication.class, args);
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+        System.out.println("Good to go...");
     }
 
 }
