392f8b483b77520baac1c8cbc75e808a103f7f54
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-14 20:24:40 -0300

    Setted up Spring Cloud configuration

--- src/main/java/com/ifsp/pwe_projetofinal/PweProjetoFinalApplication.java
+++ src/main/java/com/ifsp/pwe_projetofinal/PweProjetoFinalApplication.java
@@ -2,12 +2,13 @@ package com.ifsp.pwe_projetofinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PweProjetoFinalApplication {
 
     public static void main(String[] args) {
